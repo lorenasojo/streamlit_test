f68855da1bbdd3551b1fb1cf7a5805d46b98f366
Lorena Sojo <lorenasojo95@gamil.com>
2026-02-25 20:39:31 -0600

    Agregar animación con scipy bernoulli

--- app.py
+++ app.py
@@ -1,28 +1,37 @@
+import scipy.stats
 import streamlit as st
-import random
-import pandas as pd
+import time
 
 st.header('Lanzar una moneda')
+
+# gráfico inicial con 0.5
+chart = st.line_chart([0.5])
+
+def toss_coin(n):
+
+    trial_outcomes = scipy.stats.bernoulli.rvs(p=0.5, size=n)
+
+    mean = None
+    outcome_no = 0
+    outcome_1_count = 0
+
+    for r in trial_outcomes:
+        outcome_no += 1
+
+        if r == 1:
+            outcome_1_count += 1
+
+        mean = outcome_1_count / outcome_no
+
+        chart.add_rows([mean])
+        time.sleep(0.05)
+
+    return mean
+
 
 number_of_trials = st.slider('¿Número de intentos?', 1, 1000, 10)
 start_button = st.button('Ejecutar')
 
 if start_button:
-    results = []
-    means = []
-    
-    for i in range(1, number_of_trials + 1):
-        flip = random.choice([0, 1])
-        results.append(flip)
-        means.append(sum(results) / len(results))
-    
-    st.write(f'Experimento con {number_of_trials} intentos completado.')
-    
-    df = pd.DataFrame({
-        'iteraciones': range(1, number_of_trials + 1),
-        'resultado': results,
-        'media': means
-    })
-    
-    st.line_chart(df.set_index('iteraciones')['media'])
-    st.dataframe(df)
+    st.write(f'Experimento con {number_of_trials} intentos en curso.')
+    mean = toss_coin(number_of_trials)
